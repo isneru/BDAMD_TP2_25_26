d2bda9bd226b90f92467ccbe48020aafb982b121
Diogo Nogueira <diogoclanogueira@gmail.com>
2026-01-09 00:33:37 +0000

    technical report and fact sale FK

--- SQLScripts/DataMart/CreateFactSales.sql
+++ SQLScripts/DataMart/CreateFactSales.sql
@@ -10,5 +10,9 @@ BEGIN
         [Quantity] [bigint] NOT NULL,
         [UnitPrice] [numeric](19, 2) NOT NULL,
         [TotalAmount] [numeric](19, 2) NOT NULL,
+        CONSTRAINT [FK_FactSaleDimDate] FOREIGN KEY([DateKey]) REFERENCES [dbo].[DimDate] ([DateKey]),
+        CONSTRAINT [FK_FactSaleDimCustomer] FOREIGN KEY([CustomerKey]) REFERENCES [dbo].[DimCustomer] ([CustomerKey]),
+        CONSTRAINT [FK_FactSaleDimProduct] FOREIGN KEY([ProductKey]) REFERENCES [dbo].[DimProduct] ([ProductKey]),
+        CONSTRAINT [FK_FactSaleDimEmployee] FOREIGN KEY([EmployeeKey]) REFERENCES [dbo].[DimEmployee] ([EmployeeKey])
     ) ON [PRIMARY]
 END
